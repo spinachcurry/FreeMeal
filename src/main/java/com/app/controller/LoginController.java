cd7f0f436c4a96d46663ef50a729ee456a08f168
NamSoyan <clarus0moon@naver.com>
2024-09-24 18:20:06 +0900

    240924

--- src/main/java/com/app/controller/LoginController.java
+++ src/main/java/com/app/controller/LoginController.java
@@ -4,30 +4,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.userDTO.UserDTO;
-import com.app.userDTO.UserResultDTO;
+import com.app.userDTO.UserResultDTO; 
 import com.app.service.LoginService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
- 
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 @RequestMapping
+@Slf4j
 public class LoginController {
 	
 	private LoginService loginService;
 
-	@GetMapping("/login")
+	@GetMapping("login")
 	public String login() {
+		log.info("테스트용---");
+		
 		return "login";
 	}
-	@GetMapping("/main")
-	public String main() {
-		return "main";
-	}	
+	
     @PostMapping("/login")
     public String login(@ModelAttribute UserDTO userDTO, HttpServletRequest req) {
         UserResultDTO userResultDTO = loginService.findByUser(userDTO);  // loginService로 수정
@@ -35,8 +37,10 @@ public class LoginController {
             HttpSession http = req.getSession();
             http.setAttribute("userID", userResultDTO.getUserDTO().getUserId());
             http.setAttribute("userPW", userResultDTO.getUserDTO().getPassword());
-            http.setAttribute("userRoles", userResultDTO.getUserDTO().getUserRoles());
-            return "redirect:/";
+            //http.setAttribute("userRoles", userResultDTO.getUserDTO().getUserRoles());
+            log.info("아이디/비번 -----{}-----" + "userID" + "userPW");
+            return "main";
+            //return "redirect:/";
         }
         return "redirect:/login";
     }
